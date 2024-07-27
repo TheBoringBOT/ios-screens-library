547dc54effddaa4f2cc311cca414c983bfa2ab27
gareth-richards <gareth.richards@schibsted.com>
2024-07-27 16:54:46 +0200

    Initial Commit

---
+++ ios-screens-library/ios_screens_libraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_screens_libraryApp.swift
+//  ios-screens-library
+//
+//  Created by Gareth Richards on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct ios_screens_libraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-screens-libraryUITests/ios_screens_libraryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_screens_libraryUITestsLaunchTests.swift
+//  ios-screens-libraryUITests
+//
+//  Created by Gareth Richards on 2024-07-27.
+//
+
+import XCTest
+
+final class ios_screens_libraryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
